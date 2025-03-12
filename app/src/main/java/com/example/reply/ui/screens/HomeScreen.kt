b4b10433ded9f7ff391f5c3b276f4128799f86d7
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-03-12 12:11:41 -0400

    create home screen and add layout elements for phone

---
+++ app/src/main/java/com/example/reply/ui/screens/HomeScreen.kt
@@ -0,0 +1,172 @@
+package com.example.reply.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.reply.R
+import com.example.reply.data.Email
+import com.example.reply.data.MailboxType
+import com.example.reply.data.local.LocalEmailsDataProvider
+import com.example.reply.ui.AppUiState
+import com.example.reply.ui.theme.ReplyTheme
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHomeScreen() {
+    ReplyTheme {
+        HomeScreen(
+            appUiState = AppUiState(
+                mailboxes = LocalEmailsDataProvider.allEmails.groupBy { it.mailbox }
+            ),
+            onTabPressed = {},
+            onEmailCardClick = {}
+        )
+    }
+}
+
+@Composable
+private fun HomeScreen(
+    appUiState: AppUiState,
+    onTabPressed: (MailboxType) -> Unit,
+    onEmailCardClick: (Email) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val navigationItemContentList = listOf(
+        NavigationItemContent(
+            mailboxType = MailboxType.Inbox,
+            icon = Icons.Default.AccountBox,
+            text = stringResource(R.string.tab_inbox)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Sent,
+            icon = Icons.AutoMirrored.Filled.Send,
+            text = stringResource(id = R.string.tab_sent)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Drafts,
+            icon = Icons.Default.MailOutline,
+            text = stringResource(id = R.string.tab_drafts)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Spam,
+            icon = Icons.Default.Warning,
+            text = stringResource(id = R.string.tab_spam)
+        )
+    )
+
+    AppContent(
+        appUiState = appUiState,
+        onEmailCardClick = onEmailCardClick,
+        onTabPressed = onTabPressed,
+        navigationItemContentList = navigationItemContentList,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun AppContent(
+    appUiState: AppUiState,
+    onEmailCardClick: (Email) -> Unit,
+    onTabPressed: (MailboxType) -> Unit,
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+) {
+
+    Row(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            EmailListContent(
+                appUiState = appUiState,
+                onEmailCardClick = onEmailCardClick,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(
+                        horizontal = dimensionResource(R.dimen.email_list_only_horizontal_padding)
+                    )
+            )
+
+            AppBottomNavigationBar(
+                currentTab = appUiState.currentMailbox,
+                onTabPressed = onTabPressed,
+                navigationItemContentList = navigationItemContentList,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun AppNavigationRail(
+    currentTab: MailboxType,
+    onTabPressed: ((MailboxType) -> Unit),
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+) {
+    NavigationRail(modifier = modifier) {
+        for (navItem in navigationItemContentList) {
+            NavigationRailItem(
+                selected = currentTab == navItem.mailboxType,
+                onClick = { onTabPressed(navItem.mailboxType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun AppBottomNavigationBar(
+    currentTab: MailboxType,
+    onTabPressed: ((MailboxType) -> Unit),
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(modifier = modifier) {
+        for (navItem in navigationItemContentList) {
+            NavigationBarItem(
+                selected = currentTab == navItem.mailboxType,
+                onClick = { onTabPressed(navItem.mailboxType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                }
+            )
+        }
+    }
+}
+
+private data class NavigationItemContent(
+    val mailboxType: MailboxType,
+    val icon: ImageVector,
+    val text: String
+)
